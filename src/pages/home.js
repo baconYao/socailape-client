866dea44b9fa48e8a12e0f7a290955d07de84218
peiyao <peiyaochang@qnap.com>
2020-03-12 00:07:56 +0800

    initialized the route and scream card

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+import Scream from '../components/Scream';
 
 class home extends Component {
+  state = {
+    screams: null
+  }
+  
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          screams: res.data
+        })
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+    this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream}/>)
+    ) : <p>Loading...</p>
     return (
-      <div>
-        <h1>Home page</h1>
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={8} sx={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} sx={12}>
+          <p>profile...</p>
+        </Grid>
+      </Grid>
     )
   }
 }
